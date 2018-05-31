e2356f4d87dea6a5ff4cc3af50138c6f6b0b47e5
Tolga Arslan <tolgaarslan0@gmail.com>
2018-05-31 09:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout (string){
   return string.toUpperCase();
 }
+
+function whisper (strin){
+  return string.toLowerCase();
+}
+
+function logShout (string){
+  console.log(shout(string));
+}
+
+function logWhisper (string){
+  console.log(whisper(string));
+}
